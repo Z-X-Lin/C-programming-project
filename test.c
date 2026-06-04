332dde9372fa286322a8c13f9889e873a0119bed
Jing ran <3408775615@qq.com>
2026-06-05 07:58:13 +0800

    test

--- test.c
+++ test.c
@@ -2,23 +2,9 @@
 
 int main(void)
 {
-    loadPosts();
+    postAdd();
 
-    post *p = getPostById(1);//根据ID获取帖子，假设ID为1
-
-    if(p == NULL)
-    {
-        printf("Post Not Found!\n");
-        return 0;
-    }
-
-    printf("===== Before =====\n");
-    displayPost(p);
-
-    updatePost(p);
-
-    printf("\n===== After =====\n");
-    displayPost(p);
+    postListAll();
 
     return 0;
 }
